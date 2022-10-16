7bc2ae03db85cd39b90bcf33cf55c230a2b59ec1
Marcelo Cristiano <marceloabk@hotmail.com>
2022-10-16 16:45:29 -0300

    feat: configure ci/cd
    * feat: export lists
    
    * chore: add author to package.json
    
    * feat: configure ci/cd
    
    * feat: add lint job to workflow
    
    * feat: merge lint and test jobs

--- src/ds/index.js
+++ src/ds/index.js
@@ -1,7 +1,9 @@
 import Stack from './stack/stack.js'
 import Queue from './queue/queue.js'
 import Deque from './deque/deque.js'
+import DoubleLinkedList from './lists/doubleLinkedList/doubleLinkedList.js'
+import LinkedList from './lists/linkedList/linkedList.js'
 
 export {
-  Stack, Queue, Deque
+  Stack, Queue, Deque, LinkedList, DoubleLinkedList
 }
